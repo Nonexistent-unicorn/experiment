d47cc0ad1c74faaccdc2f3ba67a5eb2852eaa0a0
Olivia Stone <ods20a@fsu.edu>
2022-10-18 03:20:33 -0400

    sphere weight 2

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ function draw() {
 background(248,131,121);
 
     noStroke();
-
     fill(100, 20, 10);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
@@ -14,7 +13,7 @@ background(248,131,121);
 
   fill(100, 20, 10);
     stroke(248, 131, 121);
-    strokeWeight(1);
+    strokeWeight(2);
   sphere(75);
 
     fill(100, 20, 10);
@@ -28,4 +27,3 @@ background(248,131,121);
     rotateY(frameCount * 0.01);
   box(350, 350, 350)
 }
-
