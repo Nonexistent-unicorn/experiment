dfe20ce44cb229b1b1bc3058fa5e51bd042f2e40
Olivia Stone <ods20a@fsu.edu>
2022-10-18 03:10:23 -0400

    2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1525, 700, WEBGL);
 }
 
 function draw() {
-  background(125);
+background(248,131,121);
+
+    noStroke();
+
+    fill(100, 20, 10);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+  torus(150, 50);
+
+  fill(100, 20, 10);
+    stroke(248, 131, 121);
+    strokeWeight(.25);
+  sphere(75);
+
+    fill(100, 20, 10);
+    stroke(100, 20, 10);
+  torus(250, 5);
+
+    noFill()
+    stroke(100, 20, 10)
+    strokeWeight(5)
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+  box(350, 350, 350)
 }
+
