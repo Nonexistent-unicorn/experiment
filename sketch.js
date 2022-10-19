c0c1c55513d704facdc5e7e476771acb18e83aa0
Olivia Stone <ods20a@fsu.edu>
2022-10-19 11:53:48 -0400

    changed colors

--- sketch.js
+++ sketch.js
@@ -3,21 +3,23 @@ function setup() {
 }
 
 function draw() {
-background(248,131,121);
+background(0);
 
-    noStroke();
-    fill(100, 20, 10);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
+    stroke(250,50,160);
+    strokeWeight(2);
+    noFill();
+    rotateY(frameCount* 0.01);
+    rotateX(frameCount* 0.01);
   torus(150, 50);
 
-  fill(100, 20, 10);
-    stroke(248, 131, 121);
-    strokeWeight(2);
-  sphere(75);
+    fill(250,50,160);
+  
+  sphere(75)
 
     fill(100, 20, 10);
     stroke(100, 20, 10);
+    rotateX(frameCount* 0.01)
+    rotateY(frameCount* 0.01)
   torus(250, 5);
 
     noFill()
@@ -25,5 +27,10 @@ background(248,131,121);
     strokeWeight(5)
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
-  box(350, 350, 350)
+  box(350, 350, 350);
+
+ strokeWeight(.50)
+  sphere(310)
+
+
 }
